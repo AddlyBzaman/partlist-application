e529398a0422a41d53a21ec6065321c6483dd287
Addly Baddrul Jaman <62837162+AddlyBzaman@users.noreply.github.com>
2025-12-29 08:26:24 +0700

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -3,8 +3,8 @@ import mysql from 'mysql2/promise';
 // Check if environment variables are available
 const dbConfig = {
   host: process.env.DB_HOST || 'pt-nikkatsu.com',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  user: process.env.DB_USER || 'flutter',
+  password: process.env.DB_PASSWORD || 'flutter12345',
   database: process.env.DB_NAME || 'partlist',
   port: Number(process.env.DB_PORT) || 3306,
   waitForConnections: true,
